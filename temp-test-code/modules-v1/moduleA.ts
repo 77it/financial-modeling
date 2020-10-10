d051324e6ced0b067d01c48f8e76922aecd39997
Stefano Spinucci <virgo977virgo@gmail.com>
2020-10-10 16:01:14 +0200

    ETD

--- temp-test-code/modules-v1/moduleA.ts
+++ temp-test-code/modules-v1/moduleA.ts
@@ -1,5 +1,5 @@
-import { ValuesA as ValuesA1, ValuesB as ValuesB1 } from "./ExportValues2.ts";
-import { ValuesA as ValuesA2, ValuesB as ValuesB2 } from "./lib/ExportValues3.ts";
+import { ValuesA as ValuesA1, ValuesB as ValuesB1 } from "./ExportModuleValues2.ts";
+import { ValuesA as ValuesA2, ValuesB as ValuesB2 } from "./lib/ExportModuleValues3.ts";
 
 export {ValuesA1, ValuesA2, ValuesB1, ValuesB2};
 
--- temp-test-code/v1/main.ts
+++ temp-test-code/v1/main.ts
@@ -1,6 +1,3 @@
-// run with
-// deno run --allow-read --allow-write THIS.ts -i INPUT -o OUTPUT
-
 import { ValuesA1, ValuesA2, ValuesB1, ValuesB2 } from "https://raw.githubusercontent.com/stefano77it/financial-modeling/master/temp-test-code/modules-v1/moduleA.ts";
 
 console.log(ValuesA1.value + " " + ValuesA2.value + " " + ValuesB1.value + " " + ValuesB2.value);
