f5281c3838e2baf7601dd61e6846511e8e903324
Stefano Spinucci <virgo977virgo@gmail.com>
2020-10-11 00:00:54 +0200

    ETD

--- temp-test-code/v1/simulation.ps1
+++ temp-test-code/v1/simulation.ps1
@@ -2,12 +2,12 @@
 Set-Location -Path $PSScriptRoot  
 
 #region functions
-function test-and-update-deno
-{
+function test-and-update-deno {
     $last_deno_ver = "1.4.2"
-    $running_deno_ver = -split(deno --version | select-string -Pattern 'deno') | Select -Index 1 
+    $running_deno_ver = -split (deno --version | select-string -Pattern 'deno') | Select -Index 1 
 
-    if (!$?) {  # deno is not installed, or there were problems detecting the version
+    if (!$?) {
+        # deno is not installed, or there were problems detecting the version
         Write-Output "Deno missing, installing..."
         Invoke-WebRequest https://deno.land/x/install/install.ps1 -UseBasicParsing | Invoke-Expression
     }  
@@ -40,15 +40,19 @@ Add-Content -Path $fileA -Value 'Hello, World 2'  # append text to $fileA
 
 deno run --allow-read --allow-write https://raw.githubusercontent.com/stefano77it/financial-modeling/master/temp-test-code/v1/rw.ts -i $fileA -o $fileB
 
-if((Get-FileHash $fileA).hash -eq (Get-FileHash $fileB).hash)
+if ((Get-FileHash $fileA).hash -eq (Get-FileHash $fileB).hash) {
+    Write-Output "execution ended successfully"
+    Set-Content -Path $successFile -Encoding utf8 -Value 'success'
+}
+else {
+    Write-Output "execution ended in error"
+    Set-Content -Path $errorFile -Encoding utf8 -Value 'error'
+}
+
+# remove `$errorFile` if empty
+if (Test-Path $errorFile)
     {
-        Write-Output "execution ended successfully"
-        Set-Content -Path $successFile -Encoding utf8 -Value 'success'
-    }
-Else
-    {
-        Write-Output "execution ended in error"
-        Set-Content -Path $errorFile -Encoding utf8 -Value 'error'
+        if ((Get-Item $errorFile).length -eq 0) { Remove-Item $errorFile -ErrorAction Ignore }
     }
 
-#Read-Host -Prompt "execution ended, press RETURN to continue";
+# Read-Host -Prompt "execution ended, press RETURN to continue";
