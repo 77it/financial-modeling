715690d689f0fab7dd8193719ce402fa76e8072a
Stefano Spinucci <virgo977virgo@gmail.com>
2020-10-11 20:20:33 +0200

    ETD

--- temp-test-code/v1/simulation.ps1
+++ temp-test-code/v1/simulation.ps1
@@ -25,14 +25,19 @@ Write-Output "do other actions..."
 
 ############### EXTRA CODE ONLY TO TEST
 
+#region dummy actions, to remove later
 $fileA = ".\temp.txt"  # slash or backslash is the same
 $fileB = "./temp2.txt"  # slash or backslash is the same
+#endregion dummy actions, to remove later
+
 $successFile = "./ps1.success.txt"
+$warningFile = "./ps1.warning.txt"
 $errorFile = "./ps1.error.txt"
 
 Remove-Item $fileA -ErrorAction Ignore
 Remove-Item $fileB -ErrorAction Ignore
 Remove-Item $successFile -ErrorAction Ignore
+Remove-Item $warningFile -ErrorAction Ignore
 Remove-Item $errorFile -ErrorAction Ignore
 
 Set-Content -Path $fileA -Encoding utf8 -Value 'Hello, World'  # create $fileA and put some text inside
@@ -49,7 +54,7 @@ else {
     Set-Content -Path $errorFile -Encoding utf8 -Value 'error'
 }
 
-#remove `$errorFile` if empty
-if (Test-Path $errorFile) { if ((Get-Item $errorFile).length -eq 0) { Remove-Item $errorFile -ErrorAction Ignore } }
+if (Test-Path $errorFile) { if ((Get-Item $errorFile).length -eq 0) { Remove-Item $errorFile -ErrorAction Ignore } }  #remove file if empty
+if (Test-Path $warningFile) { if ((Get-Item $warningFile).length -eq 0) { Remove-Item $warningFile -ErrorAction Ignore } }  #remove file if empty
 
 #Read-Host -Prompt "execution ended, press RETURN to continue";
