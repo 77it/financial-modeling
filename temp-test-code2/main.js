9668c1c2f8dd884fb1ac6a9eef9fbb3be6cf486d
Stefano Spinucci <virgo977virgo@gmail.com>
2021-02-02 07:35:21 +0100

    E

--- temp-test-code2/main.js
+++ temp-test-code2/main.js
@@ -3,6 +3,12 @@
 // test-import-from-https
 
 import { ValuesA1, ValuesA2, ValuesB1, ValuesB2 } from "https://raw.githubusercontent.com/77it/financial-modeling/master/temp-test-code2/modules-v1/moduleA.js";
+/**
+ * @typedef { import("./types").Pet } Pet
+ */
+/**
+ * @typedef { import("https://raw.githubusercontent.com/77it/financial-modeling/master/temp-test-code2/types.d.ts").Pet } Pet2
+ */
 
 console.log(ValuesA1.value + " " + ValuesA2.value + " " + ValuesB1.value + " " + ValuesB2.value);
 
@@ -27,3 +33,18 @@ console.log(foo('a'));
 const a = ['a', '7'];
 
 console.log(a);
+
+
+/**
+ * @type {Pet}
+ */
+var myPet;
+myPet.name = "mimmo";
+console.log(myPet.name);
+
+/**
+ * @type {Pet2}
+ */
+var myPet;
+myPet.name = "mimmo2";
+console.log(myPet.name);
