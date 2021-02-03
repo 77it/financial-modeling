4ae1563103ff1ab39a6497c2d1ea1613bcd96e41
Stefano Spinucci <virgo977virgo@gmail.com>
2021-02-03 06:39:53 +0100

    etd

--- temp-test-code2/global.d.ts
+++ temp-test-code2/global.d.ts
@@ -1,2 +1,4 @@
+// used by VSCode, when processing JavaScript files
+// with activated type check with JSDOC
 // to ignore `import` error "cannot find module X or its corresponding type declarations.ts"  see https://stackoverflow.com/a/61338682/5288052
 declare module '*';
