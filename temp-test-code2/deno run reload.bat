30d5c1aceb5af7d734b758f9751a3098c63eb5d3
Stefano Spinucci <virgo977virgo@gmail.com>
2021-02-10 01:10:37 +0100

    little edits

--- temp-test-code2/deno run reload.bat
+++ temp-test-code2/deno run reload.bat
@@ -1,2 +1,6 @@
-deno run --reload --config ./tsconfig.deno.json main.js
+rmdir dist /s /q
+del src\denotypes.d.ts
+deno types >src/denotypes.d.ts
+call tsc
+deno run --reload --config ./tsconfig.deno.json ./dist/main.js
 pause
--- temp-test-code2/deno run.bat
+++ temp-test-code2/deno run.bat
@@ -1,4 +1,6 @@
 rmdir dist /s /q
+del src\denotypes.d.ts
+deno types >src/denotypes.d.ts
 call tsc
 deno run --config ./tsconfig.deno.json ./dist/main.js
 pause
