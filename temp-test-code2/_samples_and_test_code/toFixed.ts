000e83e476502d1f0aeb2f46d1327fa3bc86e156
Stefano Spinucci <virgo977virgo@gmail.com>
2021-03-20 17:26:31 +0100

    more toFixed samples

--- temp-test-code2/_samples_and_test_code/toFixed.ts
+++ temp-test-code2/_samples_and_test_code/toFixed.ts
@@ -5,3 +5,7 @@ function toFixed(num:number, decimalPlaces:number):number {
 
 console.log("9.99999 with 5 decimal places-> " + toFixed(9.99999, 5));
 console.log("9.999999 with 5 decimal places-> " + toFixed(9.999999, 5));
+console.log("0.00001 with 5 decimal places-> " + toFixed(0.00001, 5));
+console.log("0.000001 with 5 decimal places-> " + toFixed(0.000001, 5));
+console.log("-0.00001 with 5 decimal places-> " + toFixed(-0.00001, 5));
+console.log("-0.000001 with 5 decimal places-> " + toFixed(-0.000001, 5));
