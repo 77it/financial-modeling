0ef7e95c7fda4fa69ee564ef5d354b5775e3cfb2
Stefano Spinucci <virgo977virgo@gmail.com>
2021-04-03 16:58:34 +0200

    edits to IModule interface

---
+++ temp-test-code2/src/core/IModule_h.ts
@@ -0,0 +1,23 @@
+﻿// ipotesi di interfaccia TS
+
+// vedi docs, >standard_module_methods_id
+
+interface IModule {
+    oneTimeBeforeTheSimulationStarts(listOfTables: any[], setContext: Divers_and_Variables_Repository__set, getContext: Divers_and_Variables_Repository__get): void;
+
+    oneTimeAfterTheSimulationEnds(getContext: Divers_and_Variables_Repository__get): void;
+
+    beforeDailyModeling(getContext: Divers_and_Variables_Repository__get): void; // Code executed daily, before modeling starts
+
+    dailyModeling(getContext: Divers_and_Variables_Repository__get): void; // Daily modeling
+
+    afterDailyModeling(getContext: Divers_and_Variables_Repository__get): void;  // Code executed daily, after modeling ends
+}
+
+interface Divers_and_Variables_Repository__set {
+    (namespace: string, name: string, value: any): boolean;
+};
+
+interface Divers_and_Variables_Repository__get {
+    (namespace: string, name: string): any;
+};
