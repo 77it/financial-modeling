ef6cafd7d515b399cdc43aa97932fcbeff2018fd
Stefano Spinucci <virgo977virgo@gmail.com>
2021-04-11 23:08:07 +0200

    updated batch "deno run reload.bat"

--- temp-test-code2/src/lib/RuntimeMediator.ts
+++ temp-test-code2/src/lib/RuntimeMediator.ts
@@ -1,34 +1,4 @@
-// implementation of FS and ore core node.js functions
-
-import * as fs from 'fs';
-import * as readline from 'readline';
-import { IRuntimeMediator } from './RuntimeMediator_h';
-
-export class RuntimeMediator implements IRuntimeMediator {
-
-    constructor() { }
-
-    readFileSync(path: string): string {
-        return fs.readFileSync(path, 'utf8');  // see https://nodejs.org/api/fs.html#fs_fs_readfilesync_path_options
-    }
-
-    appendFileSync(path: string, contents: string): void {
-        fs.appendFileSync(path, contents);  // see https://nodejs.org/api/fs.html#fs_fs_appendfilesync_path_data_options
-    }
-
-    existsSync(path: string): boolean {
-        return fs.existsSync(path);  // see https://nodejs.org/api/fs.html#fs_fs_existssync_path
-    }
-
-    unlinkSync(path: string): void {
-        fs.unlinkSync(path);  // see https://nodejs.org/api/fs.html#fs_fs_unlinksync_path
-    }
-
-    // print a message to console, overwring the last message, padding the string with spaces for `charsNumber` times
-    // inspired to https://stackoverflow.com/a/38724334/5288052 + https://stackoverflow.com/questions/34570452/node-js-stdout-clearline-and-cursorto-functions
-    overwriteConsoleMessage(msg: string, charsNumber: number): void {
-        readline.clearLine(process.stdout, 0);
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write(msg.padEnd(charsNumber, " "));
-    }
-}
+export {}
+/*
+TODO write, inspired to "src_node\RuntimeMediator.ts"
+*/
