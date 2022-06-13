a737eb0eaec2931337f5c7919036e42449781e56
Stefano Spinucci <virgo977virgo@gmail.com>
2022-06-13 18:34:08 +0200

    .

--- simulation001/z-todelete/dynamicimport.js
+++ simulation001/z-todelete/dynamicimport.js
@@ -1,7 +1,7 @@
 // dynamic import inside a function
 export async function foo() {
     // DYNAMIC IMPORT (works with deno and browser)
-    let importUrl = "https://cdn.skypack.dev/d3-dsv";
+    let importUrl = "./dynamicimport2.js";
     const ValuesXXX = (await import(importUrl));
     return ValuesXXX;
 }
