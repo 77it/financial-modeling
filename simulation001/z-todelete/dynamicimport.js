60cc61dacdbbaeabb4e338130c1abbc8e5b5636e
Stefano Spinucci <virgo977virgo@gmail.com>
2022-06-13 23:53:41 +0200

    .

--- simulation001/z-todelete/dynamicimport.js
+++ simulation001/z-todelete/dynamicimport.js
@@ -1,7 +1,16 @@
 // dynamic import inside a function
-export async function foo() {
+export async function loadObj() {
     // DYNAMIC IMPORT (works with deno and browser)
     let importUrl = "./dynamicimport2.js";
     const ValuesXXX = (await import(importUrl));
     return ValuesXXX;
+}
+
+
+// dynamic import inside a function, choosing the object to return from a string
+export async function loadObj2(objName) {
+    // DYNAMIC IMPORT (works with deno and browser)
+    let importUrl = "./dynamicimport2.js";
+    const ValuesXXX = (await import(importUrl));
+    return ValuesXXX[objName];
 }
