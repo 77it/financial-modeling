79abf43f74112919d46f65a29fe7d2916ef0e476
Stefano Spinucci <virgo977virgo@gmail.com>
2022-06-20 15:03:52 +0200

    .

--- simulation001/modules/modules_runner.js
+++ simulation001/modules/modules_runner.js
@@ -1,39 +1,39 @@
 // dynamic import inside a function
 export async function loadObj () {
   // DYNAMIC IMPORT (works with deno and browser)
-  let importUrl = './dynamicimport2.js'
-  const ValuesXXX = (await import(importUrl))
-  return ValuesXXX
+  let importUrl = './dynamicimport2.js';
+  const ValuesXXX = (await import(importUrl));
+  return ValuesXXX;
 }
 
 // dynamic import inside a function, choosing the object to return from a string
 export async function loadObj2 (objName) {
   // DYNAMIC IMPORT (works with deno and browser)
-  let importUrl = './dynamicimport2.js'
-  const ValuesXXX = (await import(importUrl))
-  return ValuesXXX[objName]
+  let importUrl = './dynamicimport2.js';
+  const ValuesXXX = (await import(importUrl));
+  return ValuesXXX[objName];
 }
 
 export class ModulesRunner {
   /** @type {Object} */ //TODO definisci oggetto
     // contains id (URI/className) as key and a class as value
-  #classesRepo
-  #defaultClassName = 'Module'
+  #classesRepo;
+  #defaultClassName = 'Module';
 
-  static valueX = 'moduleRunner v0.1.2'
+  static valueX = 'moduleRunner v0.1.2';
 
   constructor () {
-    this.#classesRepo = {}
+    this.#classesRepo = {};
   }
 
   // method to add classes from object to the repository
   // todo implementa caricamento anche di array di oggetti; va in errore se non è definito id; value is optional
   addClassFromObject ({ moduleName, URI, classObj }) {
     if (this.#classesRepo[`${URI}/${moduleName}`] === undefined) {
-      this.#classesRepo[`${URI}/${moduleName}`] = classObj
-      return true
+      this.#classesRepo[`${URI}/${moduleName}`] = classObj;
+      return true;
     }
-    return false
+    return false;
   }
 
   // method to add classes from URI to the repository
@@ -41,14 +41,14 @@ export class ModulesRunner {
   async addClassFromURI ({ moduleName, URI }) {
     if (this.#classesRepo[`${URI}/${moduleName}`] === undefined) {
       // DYNAMIC IMPORT (works with deno and browser)
-      const _module = (await import(URI))
-      this.#classesRepo[`${URI}/${moduleName}`] = _module[this.#defaultClassName]
-      return true
+      const _module = (await import(URI));
+      this.#classesRepo[`${URI}/${moduleName}`] = _module[this.#defaultClassName];
+      return true;
     }
-    return false
+    return false;
   }
 
   getClass ({ moduleName, URI }) {
-    return this.#classesRepo[`${URI}/${moduleName}`]
+    return this.#classesRepo[`${URI}/${moduleName}`];
   }
 }
