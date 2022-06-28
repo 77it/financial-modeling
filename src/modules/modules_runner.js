3ce06a510054ec5f77b32d6c937fbc435054c6d8
Stefano Spinucci <virgo977virgo@gmail.com>
2022-06-28 11:26:11 +0200

    .

--- src/modules/modules_runner.js
+++ src/modules/modules_runner.js
@@ -101,10 +101,15 @@ export class ModulesRunner {
   /**
    Get class from the repository.
    * @param {{moduleName: string, URI: string}} p
-   * @return {undefined | {class?: *, cdnURI?: string}}
+   * @return {undefined | {class: *, cdnURI: string}}
    * */
   get ({ moduleName, URI }) {
-    return {...this.#classesRepo.get(ModulesRunner.#repoKeyBuilder(URI, moduleName))};  // return a shallow copy
+    const _ret = this.#classesRepo.get(ModulesRunner.#repoKeyBuilder(URI, moduleName));
+    if (_ret === undefined)
+      return undefined;
+    if (_ret.class == null || _ret.cdnURI == null)
+      throw new Error("internal error, some property == null or undefined");
+    return {..._ret};  // return a shallow copy
   }
 
   /**
