c5bdcf3c69ecbfdef21ae7cd8e716c2c055a06aa
77it <8860389+77it@users.noreply.github.com>
2022-08-22 12:24:02 +0200

    .

--- src/wip/driver_regex_test.ts
+++ src/wip/driver_regex_test.ts
@@ -12,59 +12,63 @@ regex
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes  // see \w (word) regex definition
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Quantifiers
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
 
-online test https://regex101.com/r/
+Online evaluator/explainer: https://regex101.com/r/    // select Javascript! (if needed)
+Graphical regex diagram: https://regexper.com/
 */
 
 import {assert, assertFalse, assertEquals, assertNotEquals} from "https://deno.land/std/testing/asserts.ts";
 
 // DRIVER, full string match
-const DRIVER_REGEX = /^[_a-zA-Z]\w*$/g;
+const DRIVER_REGEX = /^[_a-zA-Z]\w*$/;
 // $DRIVER, full string match
-const $DRIVER_REGEX = /^\$[_a-zA-Z]\w*$/g;
+const $DRIVER_REGEX = /^\$[_a-zA-Z]\w*$/;
 // $.DRIVER, full string match
-const $_DRIVER_REGEX = /^\$\.[_a-zA-Z]\w*$/g;
+const $_DRIVER_REGEX = /^\$\.[_a-zA-Z]\w*$/;
 // $.$DRIVER, full string match
-const $_$DRIVER_REGEX = /^\$\.\$[_a-zA-Z]\w*$/g;
+const $_$DRIVER_REGEX = /^\$\.\$[_a-zA-Z]\w*$/;
 // UNIT.DRIVER, full string match
-const UNIT_DRIVER_REGEX = /^[_a-zA-Z]\w*?\.[_a-zA-Z]\w*$/g;
+const UNIT_DRIVER_REGEX = /^[_a-zA-Z]\w*?\.[_a-zA-Z]\w*$/;
 
 Deno.test("driver regex tests", () => {
     //#region DRIVER_REGEX
-    const _DRIVER_REGEX_TEST_1 = "driver";
-    const _DRIVER_REGEX_TEST_2 = "_driver";
-    const _DRIVER_REGEX_TEST_3_KO = "9_driver";
+    const _DRIVER_REGEX_TEST_1 = "DrIvEr";
+    const _DRIVER_REGEX_TEST_2 = "_dri_ver";
+    const _DRIVER_REGEX_TEST_3_KO1 = "9_driver";
+    const _DRIVER_REGEX_TEST_3_KO2 = "$";
     const _DRIVER_REGEX_TEST_4 = "a";
     const _DRIVER_REGEX_TEST_5 = "_";
 
-    assertEquals(_DRIVER_REGEX_TEST_1.match(DRIVER_REGEX)?.[0], _DRIVER_REGEX_TEST_1);
-    assertEquals(_DRIVER_REGEX_TEST_2.match(DRIVER_REGEX)?.[0], _DRIVER_REGEX_TEST_2);
-    assertNotEquals(_DRIVER_REGEX_TEST_3_KO.match(DRIVER_REGEX)?.[0], _DRIVER_REGEX_TEST_3_KO);
-    assertEquals(_DRIVER_REGEX_TEST_4.match(DRIVER_REGEX)?.[0], _DRIVER_REGEX_TEST_4);
-    assertEquals(_DRIVER_REGEX_TEST_5.match(DRIVER_REGEX)?.[0], _DRIVER_REGEX_TEST_5);
+    assert(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_1));
+    assert(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_2));
+    assertFalse(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_3_KO1));
+    assertFalse(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_3_KO2));
+    assert(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_4));
+    assert(DRIVER_REGEX.test(_DRIVER_REGEX_TEST_5));
     //#endregion
 
     //#region $DRIVER_REGEX
     const _$DRIVER_REGEX_TEST_1 = "$driver";
 
-    assertEquals(_$DRIVER_REGEX_TEST_1.match($DRIVER_REGEX)?.[0], _$DRIVER_REGEX_TEST_1);
+    assert($DRIVER_REGEX.test(_$DRIVER_REGEX_TEST_1));
     //#endregion
 
     //#region $_DRIVER_REGEX
     const _$_DRIVER_REGEX_REGEX_TEST_1 = "$.driver";
 
-    assertEquals(_$_DRIVER_REGEX_REGEX_TEST_1.match($_DRIVER_REGEX)?.[0], _$_DRIVER_REGEX_REGEX_TEST_1);
+    assert($_DRIVER_REGEX.test(_$_DRIVER_REGEX_REGEX_TEST_1));
     //#endregion
 
     //#region $_$DRIVER_REGEX
     const _$_$DRIVER_REGEX_REGEX_TEST_1 = "$.$driver";
 
-    assertEquals(_$_$DRIVER_REGEX_REGEX_TEST_1.match($_$DRIVER_REGEX)?.[0], _$_$DRIVER_REGEX_REGEX_TEST_1);
+    assert($_$DRIVER_REGEX.test(_$_$DRIVER_REGEX_REGEX_TEST_1));
     //#endregion
 
     //#region UNIT_DRIVER
     const _UNIT_DRIVER_REGEX_REGEX_TEST_1 = "unit.driver";
 
-    assertEquals(_UNIT_DRIVER_REGEX_REGEX_TEST_1.match(UNIT_DRIVER_REGEX)?.[0], _UNIT_DRIVER_REGEX_REGEX_TEST_1);
+    assert(UNIT_DRIVER_REGEX.test(_UNIT_DRIVER_REGEX_REGEX_TEST_1));
     //#endregion
 });
