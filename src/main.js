497f832c1348cbf94eb71c71cc21e5b11341f1a6
77it <8860389+77it@users.noreply.github.com>
2022-10-07 13:32:04 +0200

    edit to main.js about number precision

--- src/main.js
+++ src/main.js
@@ -35,28 +35,6 @@ https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/d3/v6/index
 
 // JS ledger/SimObject
 /*
-# #digits #precision
-On Ledger (not simulation lock) are defined two public constant
-SIMULATION_NUMBERS_MAX_DIGITS = 15;
-SIMULATION_NUMBERS_DECIMAL_PLACES, set to 2  // 2 is good because is also the default number of Excel decimal digits
-
-## info about numbers, money, precision
-
-from https://mikemcl.github.io/bignumber.js/
-To aid in debugging, if BigNumber.DEBUG is true then an error will be thrown on an invalid n.
-An error will also be thrown if n is of type number with more than 15 significant digits,
-as calling toString or valueOf on these numbers may not result in the intended value.
-
-see https://en.m.wikipedia.org/wiki/Numeric_precision_in_Microsoft_Excel & https://docs.microsoft.com/en-us/office/troubleshoot/excel/floating-point-arithmetic-inaccurate-result
-Although Excel can display 30 decimal places, its precision for a specified number is confined to 15 significant figures, and calculations may have an accuracy that is even less.
-// Excel has 15 significant figures max precision, then 600.000.000.000.001 is correctly retained, instead 6.000.000.000.000.001 becomes 6.000.000.000.000.000 - is lost the final 1
-
-info about money and precision
-https://opendata.stackexchange.com/questions/10346/what-specifications-are-out-there-for-the-precision-required-to-store-money
-http://www.xbrl.org/WGN/precision-decimals-units/WGN-2017-01-11/precision-decimals-units-WGN-2017-01-11.html
-
-
-/////
 # SimObject class #SimObject
 * numbers: stored with big.js http://mikemcl.github.io/big.js/
 * numbers: throw exception with numbers with integer / decimal digits greater than SIMULATION_NUMBERS_INTEGER_PLACES & SIMULATION_NUMBERS_DECIMAL_PLACES elements (also in principal, in every number)
