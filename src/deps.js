1487572d78cf3d10de48d5eae1f13f7598c7f943
Stefano Spinucci <virgo977virgo@gmail.com>
2022-10-22 10:05:54 +0200

    add bug.js to deps,js

--- src/deps.js
+++ src/deps.js
@@ -1,1 +1,8 @@
 export { validateObj } from './lib/validation_utils.js';
+
+
+//#region number libraries
+// from https://github.com/MikeMcl/big.js/ & https://www.npmjs.com/package/big.js
+// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/big.js/index.d.ts"
+export { Big } from "https://cdn.jsdelivr.net/npm/big.js@6.2.1/big.min.mjs";
+//#endregion number libraries
