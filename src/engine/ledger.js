7ab2c6f3faee5dbc8059feda13ba4ecef5f0fca4
77it <virgo977virgo@gmail.com>
2023-01-06 03:15:40 +0100

    little update

---
+++ src/engine/ledger.js
@@ -0,0 +1,6 @@
+﻿// SimObjects storage and edits, #queue
+/*
+Don't provide a queue of future movements, because a SimObject can be changed by other modules.
+
+Store, as a property of the SimObject, an object with custom properties, as the payment plans, the interest, etc.
+ */
--- src/modules/modules_runner.js
+++ src/modules/modules_runner.js
@@ -1,3 +1,8 @@
 // .
 
 // TODO to write
+
+// closed/expired modules
+/*
+There must be a way to tell - by the modules - to ModuleRunner that a module is no longer alive and should not be called anymore.
+ */
