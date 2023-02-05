4a0288b936c581df7cc6b04c6cd43e820b92f4aa
77it <8860389+77it@users.noreply.github.com>
2023-02-05 20:15:37 +0100

    updated batch file

--- deno run main-treasury.bat
+++ deno run main-treasury.bat
@@ -1,5 +1,7 @@
 @echo off
 
+del deno.lock
+
 IF [%1] == [] GOTO EXIT_WITH_ERROR__PCT1_IS_EMPTY
 
 deno run --check --allow-net --allow-read --allow-write --allow-run %~dp0/src/main-treasury-temp.js --input %1 --output %1.trn_jsonl.tmp --errors main-treasury-temp.errors.txt
