4fc2737f9ed55451aee25da1d3c760488ffedf2e
77it <8860389+77it@users.noreply.github.com>
2023-02-15 18:01:53 +0100

    update to enum validation

--- src/engine/simobject/enums/currency_enum.js
+++ src/engine/simobject/enums/currency_enum.js
@@ -1,9 +1,12 @@
+import { deepFreeze } from '../../../lib/obj_utils.js';
+
 export const currency_enum =
   {
     UNDEFINED: "UNDEFINED",
     EUR: "EUR",
     USD: "USD"
   };
+deepFreeze(currency_enum);
 
 // @ts-ignore
 export const currency_enum_validation = Object.keys(currency_enum).map(key => currency_enum[key]);
--- src/engine/simobject/enums/doubleentryside_enum.js
+++ src/engine/simobject/enums/doubleentryside_enum.js
@@ -1,3 +1,5 @@
+import { deepFreeze } from '../../../lib/obj_utils.js';
+
 // the values are always positive, also debts and costs, then this is the sign/side (debit/credit, left/right) and the type (BS/IS) of the voice
 export const doubleEntrySide_enum =
   {
@@ -9,6 +11,7 @@ export const doubleEntrySide_enum =
     DEBUG: 'DEBUG',
     EXTRA: 'EXTRA'
   };
+deepFreeze(doubleEntrySide_enum);
 
 // @ts-ignore
 export const doubleEntrySide_enum_validation = Object.keys(doubleEntrySide_enum).map(key => doubleEntrySide_enum[key]);
