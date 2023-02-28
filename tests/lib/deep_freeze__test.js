6b1c7c09e66e58d2be8f1ea2022a699aad9dc236
77it <8860389+77it@users.noreply.github.com>
2023-02-28 02:21:44 +0100

    added test

---
+++ tests/lib/deep_freeze__test.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+
+import { deepFreeze } from '../../src/lib/obj_utils.js';
+
+import { assert, assertEquals, assertFalse, assertThrows } from '../deps.js';
+
+Deno.test('test deepFreeze()', async (t) => {
+  const a = {};
+  a.a = 99;
+  a.b = 55;
+  a.c = {};
+  a.c.a = 88;
+  a.c.b = 77;
+
+  deepFreeze(a);
+
+  // editing a property
+  assertThrows(() => {
+    a.a = 100;
+  });
+
+  // editing an inner property
+  assertThrows(() => {
+    a.c.a = 100;
+  });
+
+  // adding a property
+  assertThrows(() => {
+    a.d = 100;
+  });
+});
