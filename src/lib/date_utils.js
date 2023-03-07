0be836136a80fbf0942ef9dcbeb8a68c06559daf
77it <8860389+77it@users.noreply.github.com>
2023-03-07 14:10:37 +0100

    updated date utils comments

--- src/lib/date_utils.js
+++ src/lib/date_utils.js
@@ -46,7 +46,7 @@ function isValidDate (value) {
  * Any other input type or invalid date strings will return an `Invalid Date`.
  *
  * @param {string} argument A date string to convert, fully formed ISO8601 or YYYY-MM-DD
- * @param {*} [opt] [p.asUTC=true] If true, the date will be parsed as UTC, otherwise as local time
+ * @param {*} [opt] {undefined | {asUTC?: boolean = true}} If true, the date will be parsed as UTC, otherwise as local time
  * @returns {Date} the parsed date in the local time zone
  */
 function parseJSON (argument, opt) {
