392bb30d199d53770f1316762935097c67b72283
77it <8860389+77it@users.noreply.github.com>
2023-03-07 19:22:44 +0100

    updated Drivers. TODO drivers tests

--- src/engine/drivers/drivers.js
+++ src/engine/drivers/drivers.js
@@ -1,9 +1,9 @@
 ﻿export { Drivers };
 
-import { sanitization, validation } from '../../deps.js';
+import { sanitization } from '../../deps.js';
 import * as STD_NAMES from '../../modules/_names/standardnames.js';
 
-// TODO
+// TODO do tests
 
 class Drivers {
   /**
@@ -26,45 +26,63 @@ class Drivers {
   }
 
   /**
-   * Set a Driver
-   * @param {Object} p
-   * @param {string} [p.scenario] - Optional scenario; default is SCENARIO.BASE ('base' by now); scenario can be null, undefined or '' meaning 'base'
-   * @param {string} p.unit - Driver unit
-   * @param {string} p.name - Driver name
-   * @param {Date} [p.date] Date is optional, if missing will be set to new Date(0)
-   * @param {number} p.value - Driver value
-   * @return {boolean} true if Driver is set, false if Driver is already defined
+   * Set drivers from an array of drivers dates and values
+   * @param {{scenario?: string, unit: string, name: string, date?: Date, value: number}[]} p
+   * scenario: optional; default is SCENARIO.BASE ('base' by now); scenario can be null, undefined or '' meaning 'base'
+   * unit: driver unit
+   * name: driver name
+   * date: optional; if missing will be set to new Date(0)
+   * value: driver value
    */
-  set ({ scenario, unit, name, date, value }) {
-    const _p = sanitization.sanitizeObj({
-      obj: { scenario, unit, name, date, value },
+  set (p) {
+    sanitization.sanitizeObj({
+      obj: p,
       sanitization: {
-        scenario: sanitization.STRING_TYPE,
-        unit: sanitization.STRING_TYPE,
-        name: sanitization.STRING_TYPE,
         date: sanitization.DATE_TYPE,  // missing or invalid dates will be set to new Date(0)
         value: sanitization.NUMBER_TYPE
       }
     });
-    const _key = this.#driversRepoBuildKey({ scenario, unit, name });
-    if (!(this.#driversRepo.has(_key))) {
-      this.#driversRepo.set(_key, [{ dateMilliseconds: _p.date.getTime(), value: _p.value }]);
-    } else {
-      const _driver = this.#driversRepo.get(_key);
-      const _dateMilliseconds = _p.date.getTime();
-      // loop _driver array:
-      // 1) if the date is already present; if so, don't add it and return false.
-      // 2) if check 1) is false, insert date and value at the right position between other dates
-      for (let i = 0; i < _driver.length; i++) {
-        if (_driver[i].dateMilliseconds === _dateMilliseconds)
-          return false;
 
-        if (_driver[i].dateMilliseconds > _dateMilliseconds) {
-          _driver.splice(i, 0, { dateMilliseconds: _dateMilliseconds, value: _p.value });
-          return true;
+    // loop all entries, saving in a set the keys of the drivers that are not already present
+    const _keysToAdd = new Set();
+    for (const _item of p) {
+      const _key = this.#driversRepoBuildKey({ scenario: _item.scenario, unit: _item.unit, name: _item.name });
+      if (!(this.#driversRepo.has(_key)))
+        _keysToAdd.add(_key);
+    }
+
+    // loop all entries, adding only the drivers in the set
+    for (const _inputItem of p) {
+      const _key = this.#driversRepoBuildKey({ scenario: _inputItem.scenario, unit: _inputItem.unit, name: _inputItem.name });
+
+      if (_keysToAdd.has(_key)) {
+        if (!(this.#driversRepo.has(_key))) {
+          this.#driversRepo.set(_key, [{ dateMilliseconds: _inputItem.date.getTime(), value: _inputItem.value }]);
+        } else {
+          const _dateMilliseconds = _inputItem.date.getTime();
+          const _driver = this.#driversRepo.get(_key);
+          let _toAppendFlag = true;
+          // loop _driver array:
+          // 1) if the date is already present don't add it and set _toAppendFlag to false
+          // 2) if the date is not present insert date and value at the right position between other dates and set _toAppendFlag to false
+          // 3) if _toAppendFlag is still true, append date and value at the end of the array
+          for (let i = 0; i < _driver.length; i++) {
+            if (_driver[i].dateMilliseconds === _dateMilliseconds){
+              _toAppendFlag = false;
+              break;
+            }
+
+            if (_driver[i].dateMilliseconds > _dateMilliseconds){
+              _driver.splice(i, 0, { dateMilliseconds: _dateMilliseconds, value: _inputItem.value });
+              _toAppendFlag = false;
+            }
+          }
+          if (_toAppendFlag)
+            _driver.push({ dateMilliseconds: _dateMilliseconds, value: _inputItem.value });
         }
       }
     }
+
     throw new Error('This line should never be reached');
   }
 
@@ -74,15 +92,28 @@ class Drivers {
    * @param {string} [p.scenario] - Optional scenario; default is SCENARIO.BASE ('base' by now); scenario can be null, undefined or '' meaning 'base'
    * @param {string} p.unit - Driver unit
    * @param {string} p.name - Driver name
-   * @param {Date} [p.date] - Optional date; if missing, returns first value
+   * @param {Date} [p.date] - Optional date; if missing, returns first value; if found returns the value closest (but not greater) to the requested date
    * @return {undefined|number} Driver; if not found, returns undefined
    */
   get ({ scenario, unit, name, date }) {
+    const _date = sanitization.sanitize({ value: date, sanitization: sanitization.DATE_TYPE });  // missing or invalid dates will be set to new Date(0)
+
     const _key = this.#driversRepoBuildKey({ scenario, unit, name });
+
     if (!this.#driversRepo.has(_key))
       return undefined;
+
     const _driver = this.#driversRepo.get(_key);
-    // TODO return the right value, with the date closest (but not greater) to the requested date
+    const _dateMilliseconds = _date.getTime();  // date to search for
+    let _ret = undefined;
+
+    // search for the right driver, saving the date closest (but not greater) to the requested date
+    for (const _item of _driver) {
+      if (_item.dateMilliseconds <= _dateMilliseconds)
+        _ret = _item.value;
+    }
+
+    return _ret;
   }
 
   //#region private methods
