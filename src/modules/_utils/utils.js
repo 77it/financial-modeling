5fbfe327b81be8d64315346f509129cee87cdd5a
77it <8860389+77it@users.noreply.github.com>
2023-05-11 00:51:18 +0200

    .

--- src/modules/_utils/utils.js
+++ src/modules/_utils/utils.js
@@ -28,3 +28,8 @@ function sanitizeModuleData ({ moduleData, moduleSanitization }) {
   }
   return moduleData;
 }
+
+
+// TODO funzione di modules/utils che crea una voce di cassa a completamento dello sbilancio corrente (es VsCash)
+// con nome e tipo di default, presi da Config.
+// Parametri: ledger, unitName
