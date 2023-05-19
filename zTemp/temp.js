ac9058f313e136d84b8ffbc834ccd3074d4b7a7f
77it <8860389+77it@users.noreply.github.com>
2023-05-20 01:58:49 +0200

    temp.js

--- zTemp/temp.js
+++ zTemp/temp.js
@@ -1,7 +1,37 @@
-const replaceStr = (str, char, replacer) => {
-    const regex = new RegExp(char, "g")
-    const replaced = str.replace(regex, replacer)
-    return replaced
+/**
+ * Calculates the future value of a single cash flow
+ * @param pv - present value of the cash flow
+ * @param r - annual interest rate
+ * @param t - number of years
+ * @param m - number of compounding periods per year (if null or undefined then
+ * cash flow is calculated using continuous compounding)
+ */
+fv = function(pv, r, t, m) {
+    // Continuous compounding
+    if (m === null || m === undefined) {
+        return pv * Math.exp(r * t);
+    }
+    // Discrete compounding
+    else {
+        return pv * Math.pow(1 + r / m, m * t);
+    }
 }
 
-exports = { replaceStr };
+/**
+ * Calculates the present value of a single cash flow
+ * @param pv - future value of the cash flow
+ * @param r - annual interest rate
+ * @param t - number of years
+ * @param m - number of compounding periods per year (if null or undefined then
+ * cash flow is calculated using continuous compounding)
+ */
+pv = function(fv, r, t, m) {
+    // Continuous compounding
+    if (m === null || m === undefined) {
+        return fv / Math.exp(r * t);
+    }
+    // Discrete compounding
+    else {
+        return fv / Math.pow(1 + r / m, m * t);
+    }
+}
