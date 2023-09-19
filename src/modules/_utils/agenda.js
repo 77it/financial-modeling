6508871d97dc3754c48cc2de8b9178be59c18377
77it <8860389+77it@users.noreply.github.com>
2023-09-19 17:52:10 +0200

    updated Agenda: removed value, renamed info to data

--- src/modules/_utils/agenda.js
+++ src/modules/_utils/agenda.js
@@ -5,64 +5,42 @@ import { sanitization, validation, stripTime, toStringYYYYMMDD } from '../../dep
 class Agenda {
   /**
    Map to store things to do:
-   keys are strings of ISO serialization of date (with `toStringYYYYMMDD`), values are an array of {value: number, isSimulation: boolean, info: *}
-   * @type {Map<string, {value: number, isSimulation: boolean, info: *}[]>} */
+   keys are strings of ISO serialization of date (with `toStringYYYYMMDD`), values are an array of {isSimulation: boolean, data: *}
+   * @type {Map<string, {isSimulation: boolean, data: *}[]>} */
   #repo;
-  /**
-   Map to store last returned values for each day:
-   keys are string of ISO serialization of date (with `toStringYYYYMMDD`), values are number.
-   * @type {Map<string, number>} */
-  #lastReturnedValueForADayRepo;
-  /** @type {undefined|Date} */
+  /** @type {Date} */
   #simulation_start_date__last_historical_day_is_the_day_before;
   /** @type {undefined|Date} */
   #first_date;
 
   /** This is the Agenda class that stores things to do.
-   * Remember to set the simulation start date with `set__simulation_start_date` before setting any Agenda item.
+   * @param {{simulationStartDate: Date}} p
+   * simulationStartDate: simulation start date (the last historical day is the day before)
    */
-  constructor () {
+  constructor ({simulationStartDate}) {
     this.#repo = new Map();
-    this.#lastReturnedValueForADayRepo = new Map();
-    this.#simulation_start_date__last_historical_day_is_the_day_before = undefined;
     this.#first_date = undefined;
-  }
 
-  /** Set the simulation start date (the last historical day is the day before)
-   * @param {Date} value */
-  setSimulationStartDate (value) {
-    if (this.#simulation_start_date__last_historical_day_is_the_day_before !== undefined)  // if date is already set, throw
-      throw new Error('Agenda.set__simulation_start_date: date is already set');
-
-    validation.validate({ value: value, validation: validation.DATE_TYPE });
-
-    this.#simulation_start_date__last_historical_day_is_the_day_before = stripTime(value);
+    validation.validate({ value: simulationStartDate, validation: validation.DATE_TYPE });
+    this.#simulation_start_date__last_historical_day_is_the_day_before = stripTime(simulationStartDate);
   }
 
   /**
    * Append an Agenda item to a day, ignoring the time part of the date.
-   * If the simulation flag is true but the date is before the simulation start date, the item is ignored; and vice versa.
+   * If the simulation flag is true but the date is before the simulation start date, the item is ignored, otherwise it is added.
    * @param {Object} p
    * @param {*} p.date - Date (will be sanitized to date); if the date is invalid, throw
-   * @param {*} p.value - Numeric value of the item (will be sanitized to number); skip adding if sanitized value = 0
    * @param {boolean} p.isSimulation - True if the item is for the simulation, false if it is for the historical period
-   * @param {*} p.info - Agenda item info
+   * @param {*} p.data - Agenda item data
    * @throws {Error} if `set__simulation_start_date` is not called before
    * @throws {Error} if `date` is invalid
    */
-  set ({ date, value, isSimulation, info }) {
-    if (this.#simulation_start_date__last_historical_day_is_the_day_before === undefined)
-      throw new Error('Agenda.set: simulation_start_date__last_historical_day_is_the_day_before is undefined');
-
+  set ({ date, isSimulation, data }) {
     // validate input parameters
     validation.validateObj({
-      obj: { date, value, isSimulation, info },
-      validation: { date: validation.ANY_TYPE, value: validation.ANY_TYPE, isSimulation: validation.BOOLEAN_TYPE, info: validation.ANY_TYPE }
+      obj: { date,  isSimulation, data },
+      validation: { date: validation.ANY_TYPE, isSimulation: validation.BOOLEAN_TYPE, data: validation.ANY_TYPE }
     });
-
-    // sanitize `value` to number; if _value is 0, skip adding the item
-    const _value = sanitization.sanitize({ value: value, sanitization: sanitization.NUMBER_TYPE });
-    if (_value === 0) return;
 
     // sanitize date to Date (default date NaN); if the date is invalid, throw.
     let _date = sanitization.sanitize({ value: date, sanitization: sanitization.DATE_TYPE, options: { defaultDate: new Date(NaN) } });
@@ -78,14 +56,15 @@ class Agenda {
 
     const _key = toStringYYYYMMDD(_date);
     if (!this.#repo.has(_key)) this.#repo.set(_key, []);
-    this.#repo.get(_key)?.push({ value: _value, isSimulation, info });
+    this.#repo.get(_key)?.push({ isSimulation: isSimulation, data: data });
   }
 
   /**
-   * Get all Agenda items for a day, ignoring the time part of the date
+   * Get all Agenda items for a day, ignoring the time part of the date.
+   * Items are returned as is, without cloning.
    * @param {Object} p
    * @param {Date} p.date - Date
-   * @return {{value: number, isSimulation: boolean, info: *}[]} Agenda items
+   * @return {{isSimulation: boolean, data: *}[]} Agenda items
    */
   get ({ date }) {
     const _key = toStringYYYYMMDD(date);
--- tests/modules/utils/agenda__test.js
+++ tests/modules/utils/agenda__test.js
@@ -3,49 +3,43 @@ import { assert, assertFalse, assertEquals, assertNotEquals, assertThrows } from
 import { Agenda } from '../../../src/modules/_utils/agenda.js';
 
 Deno.test('Agenda test', async () => {
-  // create agenda
-  const agenda = new Agenda();
-
-  // set simulation start date
-  agenda.setSimulationStartDate(new Date('2020-01-01 23:59:08'));  // the time part is ignored
-  assertThrows(() => agenda.setSimulationStartDate(new Date('2020-01-01')));  // the date can't be set two times
+  // create agenda + set simulation start date (the time part is ignored)
+  const agenda = new Agenda({ simulationStartDate: new Date('2020-01-01 23:59:08') });
 
   // get items before adding any
   assertEquals(agenda.get({ date: new Date('2020-01-01') }), []);
 
   // add some simulation items
-  agenda.set({ date: '2020/01/01', value: 999, isSimulation: true, info: {} });  // string in date format
-  agenda.set({ date: new Date('2020-01-01 10:18:25'), value: '1', isSimulation: true, info: { aaa: 999 } });  // the time part is ignored, number in string format
-  agenda.set({ date: new Date('2020-01-01 23:10'), value: 888, isSimulation: false, info: {} });  // isSimulation is false, so it is ignored
-  agenda.set({ date: new Date('2020-01-01 23:10'), value: 2, isSimulation: true, info: {} });  // the time part is ignored
-  agenda.set({ date: '2020-01-02', value: 3, isSimulation: true, info: {} });  // string in date format
-  agenda.set({ date: '2020.01.04', value: 4, isSimulation: true, info: {} });  // string in date format
+  agenda.set({ date: '2020/01/01', isSimulation: true, data: { value: '999' } });  // string in date format, value is string (no sanitization is done)
+  agenda.set({ date: new Date('2020-01-01 10:18:25'), isSimulation: true, data: { aaa: 999, value: 1 } });  // the time part is ignored
+  agenda.set({ date: new Date('2020-01-01 23:10'), isSimulation: false, data: {} });  // isSimulation is false, so it is ignored
+  agenda.set({ date: new Date('2020-01-01 23:10'), isSimulation: true, data: { value: 2 } });  // the time part is ignored
+  agenda.set({ date: '2020-01-02', isSimulation: true, data: { value: 3 } });  // date in string format
+  agenda.set({ date: '2020.01.04', isSimulation: true, data: { value: 4 } });  // date in string format
   // add some historical items
-  agenda.set({ date: new Date('2019-12-31'), value: '777.05', isSimulation: false, info: {} });  // number in string format
-  agenda.set({ date: new Date('2019-12-31'), value: '777,05', isSimulation: false, info: {} });  // number in INVALID string format, ignored
-  agenda.set({ date: new Date('2019-12-31'), value: 'AAA', isSimulation: false, info: {} });  // number in INVALID string format, ignored
-  agenda.set({ date: new Date('2019-12-31 10:18:25'), value: 2, isSimulation: false, info: { aaa: 999 } });  // the time part is ignored
-  agenda.set({ date: new Date('2019-12-31 23:10'), value: 555, isSimulation: true, info: {} });  // isSimulation is true, so it is ignored
-  agenda.set({ date: new Date('2019-12-31 23:10'), value: 3, isSimulation: false, info: {} });  // the time part is ignored
+  agenda.set({ date: new Date('2019-12-31'), isSimulation: false, data: { value: 777.05 } });
+  agenda.set({ date: new Date('2019-12-31 10:18:25'), isSimulation: false, data: { aaa: 999, value: 2 } });  // the time part is ignored
+  agenda.set({ date: new Date('2019-12-31 23:10'), isSimulation: true, data: {} });  // isSimulation is true, so it is ignored
+  agenda.set({ date: new Date('2019-12-31 23:10'), isSimulation: false, data: { value: 3 } });  // the time part is ignored
 
   // get items
   assertEquals(
     agenda.get({ date: new Date('2019-12-31 08:08') }),
     [
-      { value: 777.05, isSimulation: false, info: {} },
-      { value: 2, isSimulation: false, info: { aaa: 999 } },
-      { value: 3, isSimulation: false, info: {} }
+      { isSimulation: false, data: { value: 777.05 } },
+      { isSimulation: false, data: { aaa: 999, value: 2 } },
+      { isSimulation: false, data: { value: 3 } }
     ]
   );
   assertEquals(
     agenda.get({ date: new Date('2020-01-01 08:08') }),
     [
-      { value: 999, isSimulation: true, info: {} },
-      { value: 1, isSimulation: true, info: { aaa: 999 } },
-      { value: 2, isSimulation: true, info: {} }
+      { isSimulation: true, data: { value: '999' } },
+      { isSimulation: true, data: { aaa: 999, value: 1 } },
+      { isSimulation: true, data: { value: 2 } }
     ]
   );
-  assertEquals(agenda.get({ date: new Date('2020-01-02') }), [{ value: 3, isSimulation: true, info: {} }]);
+  assertEquals(agenda.get({ date: new Date('2020-01-02') }), [{ isSimulation: true, data: { value: 3 } }]);
   assertEquals(agenda.get({ date: new Date('2020-01-03') }), []);
-  assertEquals(agenda.get({ date: new Date('2020-01-04') }), [{ value: 4, isSimulation: true, info: {} }]);
+  assertEquals(agenda.get({ date: new Date('2020-01-04') }), [{ isSimulation: true, data: { value: 4 } }]);
 });
