a13c20ffb1b0b3991afbc2f35994c7331f5b643f
77it <8860389+77it@users.noreply.github.com>
2023-09-21 13:41:13 +0200

    update to sanitization_utils

--- src/modules/_utils/sanitization_utils.js
+++ src/modules/_utils/sanitization_utils.js
@@ -15,20 +15,21 @@ function sanitizeModuleData ({ moduleData, moduleSanitization }) {
 
   validation.validateObj({ obj: moduleSanitization, validation: { tableName: validation.STRING_TYPE, sanitization: validation.OBJECT_TYPE, sanitizationOptions: validation.OBJECT_TYPE + '?' } });
 
-  // convert `moduleSanitization` to an object `tableName` as key and `sanitization` as value
+  // convert `moduleSanitization` to an object with `tableName` as key and `sanitization` as value
   /** @type {Record<string, any>} */
   const moduleSanitizationObj =  {};
   for (const _sanitization of moduleSanitization)
     moduleSanitizationObj[toStringLowerCaseTrim(_sanitization.tableName)] = _sanitization;
 
   // loop moduleData tables
-  for (const table of moduleData.tables) {
-    const _sanitization = moduleSanitizationObj[toStringLowerCaseTrim(table.tableName)];
-    // if table.tableName is found in moduleSanitization keys, sanitize with moduleSanitization.sanitization
+  for (const _currTab of moduleData.tables) {
+    // get case-insensitive `table.tableName` key from `moduleSanitizationObj` object
+    const _sanitization = moduleSanitizationObj[toStringLowerCaseTrim(_currTab.tableName)];
+    // if `table.tableName` is found in `moduleSanitizationObj` keys, sanitize `_currTab.table` with `_sanitization.sanitization`
     if (!(_sanitization == null)) {
       sanitization.sanitizeObj(
         {
-          obj: table.table,
+          obj: _currTab.table,
           sanitization: _sanitization.sanitization,
           options: _sanitization?.sanitizationOptions
         });
