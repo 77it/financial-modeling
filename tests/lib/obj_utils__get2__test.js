605f81677c586fe444eff3226118b519f502a86c
77it <8860389+77it@users.noreply.github.com>
2023-10-29 22:47:00 +0100

    more tests

--- tests/lib/obj_utils__get2__test.js
+++ tests/lib/obj_utils__get2__test.js
@@ -5,12 +5,14 @@ import { assert, assertEquals, assertFalse, assertThrows } from '../deps.js';
 Deno.test('test get2(), get keys from object in a case insensitive way', (t) => {
   const obj = {
     "a": 999,
+    "A": 888,
     "B": 555,
     " C ": 'abc'
   }
 
   assertEquals(get2(obj, 'a'), 999);
   assertEquals(get2(obj, '  A  '), 999);
+  assertEquals(get2(obj, 'A'), 888);
   assertEquals(get2(obj, '  B  '), 555);
   assertEquals(get2(obj, 'b'), 555);
   assertEquals(get2(obj, ' C '), 'abc');
