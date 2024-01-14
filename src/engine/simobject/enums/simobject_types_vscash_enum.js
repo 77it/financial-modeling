8c8c7fd673647dc33cae41becf32e367192f1036
77it <8860389+77it@users.noreply.github.com>
2024-01-14 01:47:38 +0100

    enums update

---
+++ src/engine/simobject/enums/simobject_types_vscash_enum.js
@@ -0,0 +1,15 @@
+﻿export { SimObjectTypes_VsCash_enum };
+
+import { deepFreeze, ensureArrayValuesAreUnique } from '../../../lib/obj_utils.js';
+
+const SimObjectTypes_VsCash_enum = {
+//#region SimObjectEnum_VSCASH__codeid_EXTERNAL
+BS_CASH__BANKACCOUNT_FINANCIALACCOUNT : "BS_CASH__BANKACCOUNT_FINANCIALACCOUNT",  // (ITA) Depositi_bancari_e_postali
+// skip
+// skip
+// skip
+// skip
+//#endregion SimObjectEnum_VSCASH__codeid_EXTERNAL
+}
+deepFreeze(SimObjectTypes_VsCash_enum);
+ensureArrayValuesAreUnique(Object.values(SimObjectTypes_VsCash_enum));
---
+++ src/engine/simobject/enums/simobject_types_vsnocash_enum.js
@@ -0,0 +1,21 @@
+﻿export { SimObjectTypes_VsNoCash_enum };
+
+import { deepFreeze, ensureArrayValuesAreUnique } from '../../../lib/obj_utils.js';
+
+const SimObjectTypes_VsNoCash_enum = {
+//#region SimObjectEnum_VSNOCASH__codeid_EXTERNAL
+IS_INCOME__REVALUATION : "IS_INCOME__REVALUATION",  // (ITA) Rivalutazioni
+IS_INCOME__WORKPERFORMEDBYENTITYANDCAPITALISED : "IS_INCOME__WORKPERFORMEDBYENTITYANDCAPITALISED",  // (ITA) Incrementi_di_immobilizzazioni_per_lavori_interni
+IS_EXPENSE__AMORTIZATION : "IS_EXPENSE__AMORTIZATION",  // (ITA) Ammortamenti_immobilizzazioni_immateriali
+IS_EXPENSE__DEPRECIATION : "IS_EXPENSE__DEPRECIATION",  // (ITA) Ammortamenti_immobilizzazioni_materiali
+IS_EXPENSE__EMPLOYEEEXPENSE__EMPLOYEEPROVISIONSSEVERANCEPENSIONSANDSIMILARCOMMITMENTS : "IS_EXPENSE__EMPLOYEEEXPENSE__EMPLOYEEPROVISIONSSEVERANCEPENSIONSANDSIMILARCOMMITMENTS",  // (ITA) Costi_del_personale__Trattamento_Fine_Rapporto_Trattamento_di_quiescenza_e_simili
+IS_EXPENSE__IMPAIRMENT : "IS_EXPENSE__IMPAIRMENT",  // (ITA) Svalutazioni
+IS_EXPENSE__OTHEREXPENSE__OTHERPROVISIONS : "IS_EXPENSE__OTHEREXPENSE__OTHERPROVISIONS",  // (ITA) Oneri_diversi__AltriAccantonamenti
+// skip
+// skip
+// skip
+// skip
+//#endregion SimObjectEnum_VSNOCASH__codeid_EXTERNAL
+}
+deepFreeze(SimObjectTypes_VsNoCash_enum);
+ensureArrayValuesAreUnique(Object.values(SimObjectTypes_VsNoCash_enum));
