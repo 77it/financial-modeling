879f5fed06c8e5d22e05979f5f233dff7774fad5
77it <8860389+77it@users.noreply.github.com>
2024-02-24 02:09:07 +0100

    added test.js

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+export { test2 as test };
+
+import { test } from 'node:test';
+import process from 'node:process';
+
+/**
+ * @param {string} name
+ * @param {any} testFn
+ */
+function test2(name, testFn) {
+  if (typeof Deno !== "undefined") {
+    Deno.test(name, testFn);
+  } else {
+    process.nextTick(() => {
+      test(name, testFn);
+    });
+  }
+}
