28b16f0e45e9cbc3f6e359f12d4ced08b34e21c2
77it <8860389+77it@users.noreply.github.com>
2024-03-28 23:13:33 +0100

    update sanitizeModuleData

--- src/modules/_utils/sanitization_utils.js
+++ src/modules/_utils/sanitization_utils.js
@@ -16,12 +16,14 @@ function sanitizeModuleData ({ moduleData, moduleSanitization }) {
   if (moduleData?.tables == null) return moduleData;
   if (moduleSanitization == null) return moduleData;
 
-  validateObj({ obj: moduleSanitization, validation: {
-    tableName: schema.STRING_TYPE,
-    parse: schema.OBJECT_TYPE + '?',
-    sanitization: schema.OBJECT_TYPE + '?',
-    sanitizationOptions: schema.OBJECT_TYPE + '?'
-  } });
+  validateObj({
+    obj: moduleSanitization, validation: {
+      tableName: schema.STRING_TYPE,
+      parse: schema.OBJECT_TYPE + '?',
+      sanitization: schema.OBJECT_TYPE + '?',
+      sanitizationOptions: schema.OBJECT_TYPE + '?'
+    }
+  });
 
   // loop moduleData tables
   for (const _table of moduleData.tables) {
@@ -30,46 +32,46 @@ function sanitizeModuleData ({ moduleData, moduleSanitization }) {
     /** @type {{tableName: string, parse?: *, sanitization?: *, sanitizationOptions?: *} | undefined} */
     const _pss = moduleSanitization.find(_ => eq2(_.tableName, _table.tableName));
 
-    // if `_pss` has a value, parse and sanitize `_table.table` with it
-    if (_pss != null) {
+    if (_pss == null) {
+      continue;
+    }
 
-      // if `_pss.parse` is not null and an object, parse with it
-      if (_pss.parse != null && typeof _pss.parse === 'object') {
-        // loop `_pss.parse` keys
-        for (const _parseKey of Object.keys(_pss.parse)) {
-          // loop `_table.table` rows
-          for (const _row of _table.table) {
-            // loop `_row` keys
-            for (const _rowKey of Object.keys(_row)) {
-              // if `_rowKey` matches `_parseKey` in a case insensitive way, parse `_row[_rowKey]` with `_pss.parse[_parseKey]`
-              if (eq2(_rowKey, _parseKey)) {
-                // switch over `_pss.parse[_parseKey]` values, matching them against CONST values
-                switch (_pss.parse[_parseKey]) {
-                  case CONST.YAML_PARSE:
-                    _row[_rowKey] = parseYAML(_row[_rowKey]);
-                    break;
-                  case CONST.JSON5_PARSE:
-                    _row[_rowKey] = parseJSON5(_row[_rowKey]);
-                    break;
-                  default:
-                    throw new Error(`Unknown parse type: ${_pss.parse[_parseKey]}`);
-                }
-              }
+    // if `_pss.parse` has a value and is an object, parse `_table.table` with it
+    if (_pss.parse != null && typeof _pss.parse === 'object') {
+      // loop `_pss.parse` keys
+      for (const [_parseKey, _parseType] of Object.entries(_pss.parse)) {
+        // loop `_table.table` rows
+        for (const _row of _table.table) {
+          // loop `_row` keys
+          for (const _rowKey of Object.keys(_row)) {
+            if (!eq2(_rowKey, _parseKey)) {
+              continue;
+            }
+            // if `_rowKey` matches `_parseKey` in a case insensitive way, parse `_row[_rowKey]` with `_parseType`;
+            switch (_parseType) {
+              case CONST.YAML_PARSE:
+                _row[_rowKey] = parseYAML(_row[_rowKey]);
+                break;
+              case CONST.JSON5_PARSE:
+                _row[_rowKey] = parseJSON5(_row[_rowKey]);
+                break;
+              default:
+                throw new Error(`Unknown parse type: ${_parseType}`);
             }
           }
         }
       }
+    }
 
-      // if .sanitization is not null and an object, sanitize with it
-      if (_pss.sanitization != null && typeof _pss.sanitization === 'object') {
-        sanitizeObj(
-          {
-            obj: _table.table,
-            sanitization: _pss.sanitization,
-            options: _pss?.sanitizationOptions,
-            keyInsensitiveMatch: true
-          });
-      }
+    // if .sanitization is not null and is an object, sanitize with it
+    if (_pss.sanitization != null && typeof _pss.sanitization === 'object') {
+      sanitizeObj(
+        {
+          obj: _table.table,
+          sanitization: _pss.sanitization,
+          options: _pss?.sanitizationOptions,
+          keyInsensitiveMatch: true
+        });
     }
   }
   return moduleData;
