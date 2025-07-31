8b16028d04c45486448955b1024fc1afd0df6974
77it <8860389+77it@users.noreply.github.com>
2025-07-31 23:26:23 +0200

    Implement primitive coercion handling in FmlObj class; add detailed comments for clarity.

--- src/engine/fml/fmlobj.js
+++ src/engine/fml/fmlobj.js
@@ -9,4 +9,22 @@ class FmlObj {
   get () {
     throw new Error('get() method not yet implemented');
   }
+
+  /*
+  Handles all primitive coercion with a single return value (ignoring the hint).:
+
+  String(FmlObj)  // hint 'string' -> coerces to string
+  `${FmlObj}`     // hint 'string' -> coerces to string
+  Number(FmlObj)  // hint 'number' -> coerces to number
+  +FmlObj         // hint 'number' -> coerces to number
+  FmlObj + 0      // hint 'default' -> no coercion, returns the value as is
+  0 + FmlObj      // hint 'default' -> no coercion, returns the value as is
+  FmlObj + ""     // hint 'default' -> coerces to string
+  "" + FmlObj     // hint 'default' -> coerces to string
+  FmlObj == 42    // hint 'default' -> coerces to the type of the right operand
+  FmlObj == '42'  // hint 'default' -> coerces to the type of the right operand
+   */
+  [Symbol.toPrimitive](hint) {
+    return this.get();
+  }
 }
