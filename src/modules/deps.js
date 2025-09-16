399670c9553e831248c88a1507da939809d38eab
77it <8860389+77it@users.noreply.github.com>
2025-09-16 12:35:10 +0200

    refactor: Update import for cachedParseJSON to remove deprecated function and improve module dependencies

--- src/modules/deps.js
+++ src/modules/deps.js
@@ -10,7 +10,7 @@ export { isNullOrWhiteSpace } from '../lib/string_utils.js';
 export { roundHalfAwayFromZero, roundHalfAwayFromZeroWithPrecision, truncWithPrecision } from '../lib/number_utils.js';
 export { isStringOrBooleanTrue, isStringOrBooleanFalse } from '../lib/boolean_utils.js';
 export { customParseYAML as parseYAML } from '../lib/yaml.js';
-export { cachedParseJSON5relaxed } from '../lib/json5.js';
+export { cachedParseJSONrelaxed } from '../lib/json.js';
 export { Decimal } from "../../vendor/decimaljs/decimal.js";
 export { deepFreeze, ensureArrayValuesAreUnique, eq2, get2, mergeNewKeys, sortValuesAndDatesByDate } from '../lib/obj_utils.js';
 export { tablesInfoValidation } from '../modules/_utils/tablesinfo_validation.js'
