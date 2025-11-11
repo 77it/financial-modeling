7de1f616e7b8a2de300d8516576b286e37e1e2c9
77it <8860389+77it@users.noreply.github.com>
2025-11-11 08:52:27 +0100

    comments

--- src/node/module_data_array__load_from_jsonl_file.js
+++ src/node/module_data_array__load_from_jsonl_file.js
@@ -1,3 +1,8 @@
+// We use here the standard JSON.parse ignoring the possibility to have numbers longer than what JavaScript can handle.
+// See notes below about this choice.
+
+
+
 export { moduleDataArray_LoadFromJsonlFile };
 
 import { ModuleData } from '../engine/modules/module_data.js';
@@ -24,6 +29,13 @@ function moduleDataArray_LoadFromJsonlFile (jsonlFilePath) {
   const moduleDataArray = [];
   for (const json of jsonLines) {
     if (!isNullOrWhiteSpace(json)) {
+      // We use here the standard JSON.parse ignoring the possibility to have numbers longer than
+      // what JavaScript can handle, because the source is a JSONL file exporting data from Excel
+      // so numbers are limited to what Excel can handle (15 digits), which is manageable by JavaScript too.
+      //
+      // We could use a library that converts every number to string (deserializing directly to string)
+      // but for speed reasons we avoid that here, by now.
+      // A library could be https://github.com/josdejong/lossless-json (forked in https://github.com/77it/lossless-json)
       moduleDataArray.push(new ModuleData(JSON.parse(json)));
     }
   }
