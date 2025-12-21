a20b3b439bb5a7b64174a6801f69ed547f4583f6
77it <8860389+77it@users.noreply.github.com>
2025-12-22 00:24:42 +0100

    feat: update benchmark tests to include additional parameters and improve performance metrics

--- test/vendor_test/formula/formula__bench__custom__decimal.js
+++ test/vendor_test/formula/formula__bench__custom__decimal.js
@@ -1,23 +1,24 @@
 /*
-NODEJS run
+NODEJS run on P16S with Best Performance power mode with terminal in full screen
 
-bigint sum For 1000 runs x 1,475,425 ops/sec ±3.09% (80 runs sampled)
-bigint sum with fx For 1000 runs x 1,493,350 ops/sec ±2.78% (77 runs sampled)
-bigint sum with fx & conversion from string For 1000 runs x 995 ops/sec ±2.54% (85 runs sampled)
-bigint sum generated with Eval() For 1000 runs x 1,485,614 ops/sec ±2.03% (75 runs sampled)
-sum with evaluate() formula Original Parser For 1000 runs x 6,860 ops/sec ±3.46% (82 runs sampled)
-sum with evaluate() formula Custom v7 For 1000 runs x 370,498 ops/sec ±2.09% (80 runs sampled)
-sum with COMPILED toFunction() formula Custom v7 For 1000 runs x 1,423,556 ops/sec ±1.84% (80 runs sampled)
-formula with custom variable COMPILED toFunction() formula Custom v7 For 1000 runs x 1,557 ops/sec ±1.61% (85 runs sampled)
-quick functions call COMPILED toFunction() formula Custom v7 For 1000 runs x 1,481,284 ops/sec ±2.50% (75 runs sampled)
-sum with COMPILED toFunction() formula Custom v6 For 1000 runs x 903 ops/sec ±2.14% (78 runs sampled)
-sum with COMPILED toFunction() formula Custom v5 For 1000 runs x 1,448,817 ops/sec ±3.07% (75 runs sampled)
+bigint sum For 1000 runs x 3,008,323 ops/sec ±1.04% (81 runs sampled)
+bigint sum with fx For 1000 runs x 2,912,036 ops/sec ±1.49% (91 runs sampled)
+bigint sum with type checking add For 1000 runs x 2,709,840 ops/sec ±2.08% (83 runs sampled)
+bigint sum with fx & conversion from string For 1000 runs x 1,223 ops/sec ±2.02% (93 runs sampled)
+bigint sum generated with Eval() For 1000 runs x 3,003,797 ops/sec ±1.18% (89 runs sampled)
+sum with evaluate() formula Original Parser For 1000 runs x 9,276 ops/sec ±0.72% (91 runs sampled)
+sum with evaluate() formula Custom v7 For 1000 runs x 380,073 ops/sec ±0.52% (92 runs sampled)
+sum with COMPILED toFunction() formula Custom v7 For 1000 runs x 3,433,765 ops/sec ±1.24% (92 runs sampled)
+formula with custom variable COMPILED toFunction() formula Custom v7 For 1000 runs x 3,134 ops/sec ±2.58% (92 runs sampled)
+quick functions call COMPILED toFunction() formula Custom v7 For 1000 runs x 3,557,506 ops/sec ±0.78% (92 runs sampled)
+sum with COMPILED toFunction() formula Custom v6 For 1000 runs x 1,258 ops/sec ±0.55% (96 runs sampled)
+sum with COMPILED toFunction() formula Custom v5 For 1000 runs x 2,876,786 ops/sec ±1.02% (91 runs sampled)
  */
 
 // run it with `node --import ./__node__register-hooks.js`
 // run it with `deno run --allow-read --allow-write --allow-net --allow-import`
 
-import { fxAdd, ensureBigIntScaled } from "../../../src/lib/decimal_scaled_bigint__dsb.arithmetic_x.js";
+import { fxAdd, add, ensureBigIntScaled } from "../../../src/lib/decimal_scaled_bigint__dsb.arithmetic_x.js";
 import { Parser as ParserOriginal } from "../../../vendor/formula/formula.js";
 import { Parser as ParserCustom5 } from "../../../vendor/formula/old/formula_v5_eval2cached_x.js";
 import { Parser as ParserCustom6 } from "../../../vendor/formula/old/formula_v6_eval2cached+old_behaviour_x.js";
@@ -62,24 +63,24 @@ setReferenceValues({
 
 // eval generated function
 const sumEval = eval(`
-    (function(x, y) {
-      return x + y;
+    (function(x, y, z) {
+      return x + y + z;
     })
   `);
 
 // evaluated formula original parser
-const formula_v0 = new ParserOriginal('1000000 + 2000000');
+const formula_v0 = new ParserOriginal('1000000 + 2000000 + 3000000');
 
 // preparsed formula v5
-const formula_v5 = new ParserCustom5('1000000 + 2000000');
+const formula_v5 = new ParserCustom5('1000000 + 2000000 + 3000000');
 const compiled_v5 = formula_v5.toFunction();
 
 // preparsed formula v6
-const formula_v6 = new ParserCustom6('1000000 + 2000000');
+const formula_v6 = new ParserCustom6('1000000 + 2000000 + 3000000');
 const compiled_v6 = formula_v6.toFunction();
 
 // preparsed formula v7
-const formula_v7 = new ParserCustom7('1000000 + 2000000');
+const formula_v7 = new ParserCustom7('1000000 + 2000000 + 3000000');
 const compiled_v7 = formula_v7.toFunction();
 
 // preparsed formula v7 with custom variable resolver
@@ -97,25 +98,31 @@ const expected_v7_quickFunctionCall = 500000000050n ;
 suite
   .add(`bigint sum For ${COUNTER.toLocaleString('it-IT')} runs`, function() {
     for (let i = 0; i < COUNTER; i++) {
-      const a = 1000000n + 2000000n;
+      const a = 1000000n + 2000000n + 3000000n;
     }
   })
 
   .add(`bigint sum with fx For ${COUNTER.toLocaleString('it-IT')} runs`, function() {
     for (let i = 0; i < COUNTER; i++) {
-      const a = fxAdd(1000000n, 2000000n);
+      const a = fxAdd(1000000n, fxAdd(2000000n, 3000000n));
+    }
+  })
+
+  .add(`bigint sum with type checking add For ${COUNTER.toLocaleString('it-IT')} runs`, function() {
+    for (let i = 0; i < COUNTER; i++) {
+      const a = add(1000000n, add(2000000n, 3000000n));
     }
   })
 
   .add(`bigint sum with fx & conversion from string For ${COUNTER.toLocaleString('it-IT')} runs`, function() {
     for (let i = 0; i < COUNTER; i++) {
-      const a = fxAdd(ensureBigIntScaled("1000000"), ensureBigIntScaled("2000000"));
+      const a = fxAdd(ensureBigIntScaled("1000000"), fxAdd(ensureBigIntScaled("2000000"), ensureBigIntScaled("3000000")));
     }
   })
 
   .add(`bigint sum generated with Eval() For ${COUNTER.toLocaleString('it-IT')} runs`, function() {
     for (let i = 0; i < COUNTER; i++) {
-      const a = sumEval(1000000n, 2000000n);
+      const a = sumEval(1000000n, 2000000n, 3000000n);
     }
   })
 
